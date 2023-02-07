1bf262eb0000b50ef4a22b72ba083310986c4499
Nanta Arthur <109583020+NantaArthur123@users.noreply.github.com>
2023-02-07 18:25:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ function checkinput() {
         document.getElementById("hasil_salin").style.visibility = "visible";
         document.getElementById("hasil_container_2").style.visibility = "visible";
         document.getElementById("hasil_salin_2").style.visibility = "visible";
-        return ["https://faruqkiki.my.id/?name="+document.getElementById("nama_undangan").value.replace(/ /g,"%20"),"https://kikifaruq.my.id/?name="+document.getElementById("nama_undangan").value.replace(/ /g,"%20")];
+        var undanganfull  = text_builder(document.getElementById("nama_undangan").value)
+        return ["https://faruqkiki.my.id/?name="+document.getElementById("nama_undangan").value.replace(/ /g,"%20"),undanganfull];
     }
 }
 
@@ -44,4 +45,11 @@ function salin() {
   
     // Alert the copied text
     alert("Copied the text: " + copyText);
+  }
+
+
+
+  function text_builder(name){
+    var x = "*UNDANGAN*<br> <br> Kepada Yth:<br> sdr/i "+name+"<br> <br> Bismillahirahmanirrahim.<br> Assalamualaikum Warohmatullohi wabarokaatuh<br> <br> Tanpa mengurangi rasa hormat, perkenankan kami mengundang sdr/i "+name+" untuk menghadiri acara Tasyakuran Pernikahan putra dan putri kami:<br> <br> *Mohammad Musthofa Kamal Alfaruqi dan Nur Rizqi Aini*<br> <br> dengan link e-invitation sebagai berikut<br> <br> <br> https://faruqkiki.my.id/?name="+name.replace(/ /g,"%20")+"<br> <br> Merupakan suatu kebahagiaan bagi kami apabila berkenan hadir dan memberikan doa terbaik serta restu kepada putra putri kami tersebut.<br> <br> Mohon maaf bila undangan hanya berupa e-invitation.<br> <br> Salam dan Hormat Kami,<br> Keluarga HM Chusnul Yaqin <br> <br> Wassalamualaikum Warohmatullohi&#160;Wabarokaatuh";
+    return x;
   }
